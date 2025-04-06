35472dc08292c752902658d4b589ca998bb1e5be
Pammi-Dharewa <dharewapammi@gmail.com>
2025-04-06 13:24:17 +0530

    push

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 import HeroSection from '../components/home/HeroSection'
-import IntroSection from '../components/home/IntroSEction'
 import FeaturesSection from '../components/home/FeaturesSection'
 import ReviewsSection from '../components/home/ReviewsSection'
 import FacultySection from '../components/home/FacultySection'
+import IntroSection from '../components/home/IntroSection'
 
 const Index = () => {
   return (
